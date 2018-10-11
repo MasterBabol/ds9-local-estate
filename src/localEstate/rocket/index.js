f290ef03b88fa4a472a3c42ad5932ffb3737f742
MasterBabol <babol@live.co.kr>
2018-10-12 01:14:56 +0900

    Add missed db var assign

--- src/localEstate/rocket/index.js
+++ src/localEstate/rocket/index.js
@@ -94,6 +94,7 @@ const dispatchRxqueue = async (leCtx) => {
 const dispatchTxqueue = async (leCtx) => {
     let txReqs = await leCtx.rcon.send('/dequeue_tx_queue');
     let txReqsParsed = JSON.parse(txReqs);
+    let db = leCtx.db.read();
     let prevFailedTx = db.defaults({ 'failed-tx-inv': {} }).get('failed-tx-inv');
     let itemsQuery = {};
 
